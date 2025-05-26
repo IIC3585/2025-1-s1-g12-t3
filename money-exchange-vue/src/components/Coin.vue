66205eb24ad0115da29b62e22a2962758749443f
Matías Adauy <adauym@gmail.com>
2025-05-25 21:19:00 -0400

    added vue version

---
+++ money-exchange-vue/src/components/Coin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="
+      w-10 h-10
+      rounded-full
+      bg-gradient-to-br from-yellow-300 to-yellow-400
+      border-4 border-yellow-300/20
+      shadow-inner
+      font-bold tracking-wide
+      flex items-center justify-center
+      ring-1 ring-yellow-500/20
+      transition-all duration-300
+    "
+    style="font-size: 0.65rem"
+    :title="text"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  text: ""
+});
+</script>
